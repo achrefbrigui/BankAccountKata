d61362395721a23488545eb77e0cfdf72ae3a147
Achref <achref.brigui98@gmail.com>
2022-03-30 00:32:46 +0200

    Exceptions add

---
+++ src/BankAccount.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string entity) : base($"Entity {entity} was not found")
+        {
+
+        }
+    }
+}
