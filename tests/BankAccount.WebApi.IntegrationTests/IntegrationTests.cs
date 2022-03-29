61ccabfcb52c0b6629a7c41835c52a4d9c711d4d
Achref <achref.brigui98@gmail.com>
2022-03-29 02:05:54 +0200

    Integration project test init

---
+++ tests/BankAccount.WebApi.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,29 @@
+﻿using BankAccount.Application.Interfaces.Repositories;
+using BankAccount.Application.Interfaces.Services;
+using BankAccount.Application.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankAccount.WebApi.IntegrationTests
+{
+    public partial class IntegrationTests : IClassFixture<WebAppFactoryTest>
+    {
+        private readonly HttpClient _client;
+        private readonly AccountService _accountService;
+        private readonly Mock<IAccountService> _accountServiceMock = new Mock<IAccountService>();
+        private readonly Mock<IAccountRepo> _accountRepoMock = new Mock<IAccountRepo>();
+
+        public IntegrationTests(WebAppFactoryTest factory)
+        {
+            _client = factory.CreateClient();
+            _accountService = new AccountService(_accountRepoMock.Object);
+        }
+    }
+}
---
+++ tests/BankAccount.WebApi.IntegrationTests/WebAppFactoryTest.cs
@@ -0,0 +1,32 @@
+﻿using BankAccount.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankAccount.WebApi.IntegrationTests
+{
+    public class WebAppFactoryTest : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                object p = services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+                var sp = services.BuildServiceProvider();
+
+                using var scope = sp.CreateScope();
+
+                var scopedServices = scope.ServiceProvider;
+
+                var db = scopedServices.GetRequiredService<AppDbContext>();
+                db.Account.Add(new Domain.Models.AccountModel());
+                db.SaveChanges();
+            }
+            );
+        }
+    }
+}
