493414b6d9c8f12ef98e4d9c2e4e25552324ec60
Achref <achref.brigui98@gmail.com>
2022-03-29 02:13:19 +0200

    services extensions add

---
+++ src/BankAccount.Application/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using BankAccount.Application.Interfaces.Services;
+using BankAccount.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAccountService, AccountService>();
+        }
+    }
+}
---
+++ src/BankAccount.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BankAccount.Application.Interfaces.Repositories;
+using BankAccount.Infrastructure.Persistence;
+using BankAccount.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("BankAccountInMemoryDb"));
+            services.AddScoped(typeof(IGenericRepo<>), typeof(GenericRepo<>));
+            services.AddScoped<IAccountRepo, AccountRepo>();
+        }
+    }
+}
