37d36670c43a00cfe0e4fb4bdcaead0c9bd88b33
Achref <achref.brigui98@gmail.com>
2022-03-29 02:56:53 +0200

    infa service extensions

--- src/BankAccount.Infrastructure/ServiceExtensions.cs
+++ src/BankAccount.Infrastructure/ServiceExtensions.cs
@@ -3,11 +3,6 @@ using BankAccount.Infrastructure.Persistence;
 using BankAccount.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankAccount.Infrastructure
 {
