72ce8e36d033d10f8e3270f9e206916fd737b7b1
achrefbrigui <62870799+achrefbrigui@users.noreply.github.com>
2023-11-03 12:05:14 +0100

    Update AccountController.cs
    Add comments

--- src/BankAccount.WebApi/Controllers/AccountController.cs
+++ src/BankAccount.WebApi/Controllers/AccountController.cs
@@ -28,6 +28,11 @@ namespace BankAccount.WebApi.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Add an account
+        /// </summary>
+        /// <param name="command">Initial account balance</param>
+        /// <returns></returns>
         [HttpPost]
         [Route("Account")]
         public async Task<IActionResult> AddAccount([FromBody] AddAccountCommand command)
@@ -35,6 +40,10 @@ namespace BankAccount.WebApi.Controllers
             return Ok((await _mediator.Send(command)).Success());
         }
 
+        /// <summary>
+        /// Get all accounts
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("Account")]
         public async Task<IActionResult> GetAllAccounts()
@@ -42,6 +51,11 @@ namespace BankAccount.WebApi.Controllers
             return Ok((await _mediator.Send(new GetAllAccountsQuery())).Success());
         }
 
+        /// <summary>
+        /// Get an account by id
+        /// </summary>
+        /// <param name="id">Account id</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("Account/{id}")]
         public async Task<IActionResult> GetAccountById(string id)
@@ -49,6 +63,12 @@ namespace BankAccount.WebApi.Controllers
             return Ok((await _mediator.Send(new GetAccountByIdQuery() { Id = id })).Success());
         }
 
+        /// <summary>
+        /// Add an operation to an account
+        /// </summary>
+        /// <param name="id">Account id</param>
+        /// <param name="command">Operation type and amount</param>
+        /// <returns></returns>
         [HttpPost]
         [Route("Account/{id}/Operations")]
         public async Task<IActionResult> AddOperation(string id, [FromBody] AddOperationCommand command)
@@ -57,6 +77,11 @@ namespace BankAccount.WebApi.Controllers
             return Ok((await _mediator.Send(command)).Success());
         }
 
+        /// <summary>
+        /// Retrieve the operations of an account
+        /// </summary>
+        /// <param name="id">Account id</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("Account/{id}/Operations")]
         public async Task<IActionResult> GetAccountOperations(string id)
@@ -64,6 +89,13 @@ namespace BankAccount.WebApi.Controllers
             return Ok((await _mediator.Send(new GetAccountOperationsQuery() { UserId = id })).Success());
         }
 
+        /// <summary>
+        /// Retrieve the operations of an account as a paginated response
+        /// </summary>
+        /// <param name="id">Account id</param>
+        /// <param name="pageNb">Page nb</param>
+        /// <param name="pageSize">Page size</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("Account/{id}/PaginatedOperations")]
         public async Task<IActionResult> GetAccountPaginatedOperations(string id, [FromQuery] int pageNb, [FromQuery] int pageSize)
